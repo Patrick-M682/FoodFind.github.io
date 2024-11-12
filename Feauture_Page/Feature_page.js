beaa45740ab78c171952a2dd073076620ed085d5
Patrick-M682 <pmore006@uottawa.ca>
2024-11-12 16:06:16 -0500

    Updated feature page

--- Feauture_Page/Feature_page.js
+++ Feauture_Page/Feature_page.js
@@ -1,12 +1,12 @@
 function toggleTextBox(textBoxId) {
-    var textBox = document.getElementById(textBoxId);
-    
-    // Check if the text box is currently visible
-    if (textBox.style.display === "none" || textBox.style.display === "") {
-      // Show the text box
-      textBox.style.display = "block";
-    } else {
-      // Hide the text box
-      textBox.style.display = "none";
-    }
+  var textBox = document.getElementById(textBoxId);
+  
+  // Check if the text box is currently visible
+  if (textBox.style.display === "none" || textBox.style.display === "") {
+    // Show the text box
+    textBox.style.display = "block";
+  } else {
+    // Hide the text box
+    textBox.style.display = "none";
   }
+}
